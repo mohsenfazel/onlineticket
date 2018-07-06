5d78e07577e8cc01a2987b7b27a642090cb05ec2
setare behzadi <setare.behzadi@gmail.com>
2018-07-06 16:29:50 +0000

    Constants

---
+++ constants.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: setare
+ * Date: 7/5/2018
+ * Time: 11:07 PM
+ */
+//it should be defined in the beginning of functions file.
+define('wpx_dir',get_template_directory());        //return path from root to where file is executed.
+define('wpx_url',get_template_directory_uri());    //return path Url
+
+
+//var_dump('wpx_dir');
+
