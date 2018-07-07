37521c0511d631f81c396ac7bfc3f740b8d8b6eb
[samaneh afsari] <sam.afsari@yahoo.com>
2018-07-07 18:21:33 +0430

    add style adn functions files

---
+++ Autoloader.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: setare
+ * Date: 7/5/2018
+ * Time: 7:02 PM
+ */
+//in the function file we need to write"include "Autoloader.php"
+//in the function file we need to write "spl_autoload_register(Autoloader::autoload);
+class Autoloader
+{
+
+    public static function autoload($class)
+    {
+        $classParts= explode('\\',$class);
+        $className = implode('/',$classParts);
+        $className = wpx_dir.DIRECTORY_SEPARATOR.$className.'.php';
+//        var_dump($className);
+//        exit();
+        
+        if (file_exists($className))
+        {
+            include_once $className;
+        }
+
+    }
+
+}
---
+++ partials/footer-bottom-grids.php
@@ -0,0 +1,59 @@
+<div class="footer-bottom-grids">
+	<!-- container -->
+	<div class="container">
+		<div class="footer-bottom-top-grids">
+			<div class="col-md-4 footer-bottom-left">
+				<h4>Download our mobile Apps</h4>
+				<div class="d-apps">
+					<ul>
+						<li><a href="#"><img src="images/app1.png" alt="" /></a></li>
+						<li><a href="#"><img src="images/app2.png" alt="" /></a></a></li>
+						<li><a href="#"><img src="images/app3.png" alt="" /></a></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-4 footer-bottom-left">
+				<h4>We Accept</h4>
+				<div class="a-cards">
+					<ul>
+						<li><a href="#"><img src="images/c1.png" alt="" /></a></li>
+						<li><a href="#"><img src="images/c2.png" alt="" /></a></a></li>
+						<li><a href="#"><img src="images/c3.png" alt="" /></a></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-4 footer-bottom-left">
+				<h4>Follow Us</h4>
+				<div class="social">
+					<ul>
+						<li><a href="#" class="facebook"> </a></li>
+						<li><a href="#" class="facebook twitter"> </a></li>
+						<li><a href="#" class="facebook chrome"> </a></li>
+						<li><a href="#" class="facebook dribbble"> </a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+			<div class="copyright">
+				<p>Copyrights © 2015 Govihar . Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+<script defer src="js/jquery.flexslider.js"></script>
+<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+<script src="js/jquery-ui.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript">
+    $(function(){
+        SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+        $('.flexslider').flexslider({
+            animation: "slide",
+            start: function(slider){
+                $('body').removeClass('loading');
+            }
+        });
+    });
+</script>
