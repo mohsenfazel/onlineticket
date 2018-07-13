b1da4de8999d21e41be3d8b5c56223e859c061b0
Ali Reza Bayani <areza.bayani@gmail.com>
2018-07-13 18:42:24 +0000

    move text

---
+++ partials/move-text.php
@@ -0,0 +1,9 @@
+
+<div class="move-text">
+		<div class="marquee">Register your hotel with us free of cost.<a href="signup.html">Here</a></div>
+		<script type="text/javascript" src="js/jquery.marquee.min.js"></script>
+        <script>
+         $('.marquee').marquee({ pauseOnHover: true });
+		  //@ sourceURL=pen.js
+		</script>
+	</div>
