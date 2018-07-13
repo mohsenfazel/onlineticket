47b3c11856d35e8194f3d0e08a2a4dd49ae09bc0
Ali Reza Bayani <areza.bayani@gmail.com>
2018-07-13 18:46:50 +0000

    index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<?php get_template_part( 'partials/top-header' ); ?>
+<?php get_template_part( 'partials/main-content' ); ?>
+<?php get_template_part( 'partials/move-text' ); ?>
+<?php get_template_part( 'partials/move-text' ); ?>
+<?php get_template_part( 'partials/banner-bottom' ); ?>
+<?php get_template_part( 'partials/popular-grids' ); ?>
+<?php get_template_part( 'partials/footer' ); ?>
+<?php get_template_part( 'partials/footer-bottom-grids' ); ?>
+<?php get_footer(); ?>
