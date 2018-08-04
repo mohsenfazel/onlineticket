1eb6b07b70aa3c4310ba2c5b09ecb68dfe4fcc3c
SetarehBehzadi <setare.behzadi@gmail.com>
2018-08-04 21:40:20 +0430

    css and js

--- functions.php
+++ functions.php
@@ -3,3 +3,5 @@ include "constants.php";
 include "Autoloader.php";
 spl_autoload_register('Autoloader::autoload');
 add_action('init','Application\Content\PostTypes\WpxPostTypes::init');
+
+add_action('wp_enqueue_scripts','Application\Asset::load_assets'); /** define all css and javascripts in class Asset with two method */
